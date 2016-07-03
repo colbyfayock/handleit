37205587b502d539af288a531eb7d7b01fc21300
Colby Fayock <fayock@gmail.com>
2016-07-03 18:16:01 -0400

    event delgation

--- handleit/handleit.js
+++ handleit/handleit.js
@@ -13,20 +13,23 @@ $(function() {
 
     handle_trigger = function(handler) {
 
-        var $that = handler.$that;
+        var $that = handler.$that,
+            target = handler.target && handler.target !== '' ? handler.target : false;
 
-        $that.on(handler.event, function(event) {
+        $that.on(handler.event, target, function(event) {
 
             event.trigger = handler.trigger;
             $that.trigger(event.trigger, [event]);
 
-            if ( handler.return && handler.return !== '' ) {
-
-                if ( handler.return === 'false' ) {
-                    return false;
-                }
-
+            if ( handler.return === 'preventDefault' ) {
+                event.preventDefault();
             }
+
+            if ( handler.return === 'stopPropagation' ) {
+                event.stopPropagation();
+            }
+
+            if ( handler.return === 'false' ) return false;
 
         });
     }
@@ -37,34 +40,33 @@ $(function() {
             event = $that.attr('data-event'),
             trigger = $that.attr('data-trigger'),
             handler = $that.attr('data-handler'),
+            target = $that.attr('data-target'),
             event_return = $that.attr('data-return'),
-            handler_events;
+            handler_events = [];
 
         if ( typeof handle_trigger !== 'function' ) return false;
 
-        if ( handler && handler !== '' ) {
-
-            handler_events = JSON.parse(handler);
-
-            for ( var i = 0, handler_events_len = handler_events.length; i < handler_events_len; i++ ) {
-                handle_trigger( {
-                    $that: $that,
-                    event: handler_events[i].event,
-                    trigger: handler_events[i].trigger,
-                    return: event_return
-                });
-            }
-
-        } else if ( event && event !== '' ) {
-            handle_trigger( {
+        if ( event && event !== '' ) {
+            handler_events.push({
                 $that: $that,
                 event: event,
                 trigger: trigger,
+                target: target,
+                return: event_return
+            });
+        } else if ( handler && handler !== '' ) {
+            handler_events = JSON.parse(handler);
+        }
+
+        for ( var i = 0, handler_events_len = handler_events.length; i < handler_events_len; i++ ) {
+            handle_trigger({
+                $that: $that,
+                event: handler_events[i].event,
+                trigger: handler_events[i].trigger,
+                target: handler_events[i].target,
                 return: event_return
             });
         }
-
-
 
     };
 
